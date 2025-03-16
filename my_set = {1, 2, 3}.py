1c9d9fa13216803395f1feee46936de84c66f0ad
RANKblank36 <tendosana@gmail.com>
2025-03-16 17:59:30 +0200

    Add files via upload

---
+++ my_set = {1, 2, 3}.py
@@ -0,0 +1,17 @@
+my_set = {1, 2, 3}
+print(my_set)
+
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+
+my_set = set([1, 2, 3, 2])
+print(my_set, "\n")
+
+my_set = set([0, 1, 3, 4, 5])
+print("Original set:")
+print(my_set)
+print("After reviewing the first element from the said set:")
+print(my_set, "\n")
